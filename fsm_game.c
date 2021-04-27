c27bea49dc0593dbaaf7b60bcc250bbdac24e0fb
wardgeronimussmets <81919921+wardgeronimussmets@users.noreply.github.com>
2021-04-27 08:31:59 +0200

    BASE framework
    builds successfully

--- fsm_game.c
+++ fsm_game.c
@@ -17,12 +17,24 @@
 #define PUSHED 0
 #define RELEASED 1
 
+#define HIGH 1
+#define LOW 0
+#define TRUE 1
+#define FALSE 0
+
+#define RedLDWait 3000
+#define GreenLDWait 5000
+
 /** P R I V A T E   V A R I A B L E S *******************************/
+static unsigned char greenLDWasOn = TRUE;
+static unsigned char gear1 = 0;
+static unsigned char gear2 = 0;
+static unsigned char newGear1 = 0;
+static unsigned char newGear2 = 0;
 
+static unsigned int counter = 0;
 
-
-
-static enum {FSM_IDLE,FSM_INITIALISE,FSM_GO,FSM_CARS_FORWARD,FSM_CARS,BREAK,FSM_CARS_BURST,FSM_CARS_BACKWARDS
+static enum {FSM_IDLE,FSM_INITIALISE,FSM_GO,FSM_FORWARD,FSM_CARS,BREAK,FSM_CARS_BURST,FSM_BACKWARDS,FSM_GAMEOVER
             } current_state;
             
 /********************************************************************
@@ -35,6 +47,17 @@ static enum {FSM_IDLE,FSM_INITIALISE,FSM_GO,FSM_CARS_FORWARD,FSM_CARS,BREAK,FSM_
  ********************************************************************/
 void fsm_game_init(void) {
 	current_state = FSM_IDLE;
+  
+    counter = 0;
+    DC2Fw_out = LOW;
+    DC1Bw_out = LOW;
+    DC2Bw_out = LOW;
+    BDLED1_out = LOW;
+    BDLED2_out = LOW;
+    CONTR_VU1 = LOW;
+    CONTR_VU2 = LOW;
+    LEDGr_out = LOW;
+    LEDRed_out = LOW;
 }
 
 /********************************************************************
@@ -46,18 +69,98 @@ void fsm_game_init(void) {
  ********************************************************************/
 void fsm_game(void) {
     
-    switch (current_state) { 
-        case FSM_IDLE :
-        // *** outputs ***
-            //led1_output = 1;
+    switch (current_state) {                
+        case FSM_IDLE:
+            /*DC1Fw_out = LOW;
+            DC2Fw_out = LOW;
+            DC1Bw_out = LOW;
+            DC2Bw_out = LOW;
+            BDLED1_out = LOW;
+            BDLED2_out = LOW;
+            CONTR_VU1 = LOW;
+            CONTR_VU2 = LOW;
+            LEDGr_out = LOW;
+            LEDRed_out = LOW;*/
             
-        // *** transitions ***
-            if (PRG_BUTTON == PUSHED) current_state = FSM_IDLE;
-            break;
-
+            if(CONT1_CLUTCH == PUSHED && CONT2_CLUTCH == PUSHED)
+            {
+                current_state = FSM_INITIALISE;
+                counter = 0;
+            }
+        case FSM_INITIALISE:
+            /*DC1Fw_out = LOW;
+            DC2Fw_out = LOW;
+            DC1Bw_out = LOW;
+            DC2Bw_out = LOW;
+            BDLED1_out = LOW;
+            BDLED2_out = LOW;
+            CONTR_VU1 = LOW;
+            CONTR_VU2 = LOW;
+            LEDGr_out = LOW;*/
+            LEDRed_out = HIGH;
+            
+            counter ++;
+            if(counter > RedLDWait)
+            {
+                current_state = FSM_GO;
+                counter = 0;
+            }
+            
+        case FSM_GO:
+            LEDRed_out = LOW;
+            LEDGr_out = HIGH;
+            greenLDWasOn = FALSE;
+            current_state = FSM_FORWARD;
+            
+       case FSM_FORWARD:
+           //check if green led needs to be on
+           if(!greenLDWasOn)
+           {
+               counter ++;
+               if(counter > GreenLDWait)
+               {
+                   greenLDWasOn = TRUE;
+                   LEDGr_out = LOW;
+               }
+           }   
+           //check if a car has finished
+           if(ENDLOOP_FinishS == PUSHED)
+               current_state = FSM_GAMEOVER;
+           
+           
+           //check if car has shifted a gear          
+        newGear1 = 0;
+        if(CONT1_GEAR1 == PUSHED)  newGear1 =  1;
+        if(CONT1_GEAR2 ==  PUSHED) newGear1 =  2;
+        if(CONT1_GEAR3 ==  PUSHED) newGear1 =  3;
+        if(CONT1_GEAR4 ==  PUSHED) newGear1 =  4;
+        if(CONT1_GEAR5 ==  PUSHED) newGear1 =  5;
+        if(CONT1_GEAR6 ==  PUSHED) newGear1 =  6;
+    
+        newGear2 = 0;
+        if(CONT2_GEAR1 ==  PUSHED) newGear2 =  1;
+        if(CONT2_GEAR2 ==  PUSHED) newGear2 =  2;
+        if(CONT2_GEAR3 ==  PUSHED) newGear2 =  3;
+        if(CONT2_GEAR4 ==  PUSHED) newGear2 =  4;
+        if(CONT2_GEAR5 ==  PUSHED) newGear2 =  5;
+        if(CONT2_GEAR6 ==  PUSHED) newGear2 =  6;
+        
+        if(newGear1 == gear1)
+        {
+            //gear1 no gear change
+            
+        }
+        else
+        {
+            //gear1 has been changed
+        }
+    
+           
         default:
             current_state = FSM_IDLE;
             break;
     }
+    
+    
     
 }
--- fsm_io.c
+++ fsm_io.c
@@ -11,15 +11,14 @@
 /** I N C L U D E S *************************************************/
 #include "config.h"
 
+/**DEFINES*/
+#define HIGH 1
+#define LOW 0
+#define PRESSED 0
+#define RELEASED 1
+
 /** P R I V A T E   V A R I A B L E S *******************************/
-static unsigned char servo_counter;
-static unsigned char stepper_counter;
-static unsigned char pwm_counter;
-static enum {FSM_SETIO,
-             FSM_UPDATE_SERVO,
-             FSM_UPDATE_STEPPER_L,
-             FSM_UPDATE_STEPPER_H,
-            } current_state;
+
 static enum {MULT_A0,MULT_A1,MULT_A2,MULT_A3,MULT_A4,MULT_A5,MULT_A6,MULT_A7,MULT_None}current_mult;
             
 /********************************************************************
@@ -31,7 +30,6 @@ static enum {MULT_A0,MULT_A1,MULT_A2,MULT_A3,MULT_A4,MULT_A5,MULT_A6,MULT_A7,MUL
  *                  may initialize some counters          
  ********************************************************************/
 void fsm_io_init(void) {
-	current_state = FSM_SETIO;
     current_mult = MULT_A0;
     
     
@@ -48,46 +46,95 @@ void fsm_io_init(void) {
  ********************************************************************/
 void fsm_io(void) {
     
-    switch (current_state) {                
-        
-            
-        default:
-            current_state = FSM_SETIO;
-            break;
-    }
+  
     
     //Multiplexing
     switch(current_mult){
         
         case MULT_A0://connected to a start end loop switch and the end loop switch
         
-            if(PRG_BUTTON==1)
-            {
-                ENDLOOP_StartS = 1;
-                
-            }
-            if(CONTR_OUT1>1)
-            {
-                ENDLOOP_StartS = 1;
-            }
+            ENDLOOP_StartS1 = CONTR_OUT1;
+            ENDLOOP_FinishS = CONTR_OUT1;
             
-            
+            MULT_S0 = HIGH;
+            MULT_S1 = LOW;
+            MULT_S2 = LOW;
+            MULT_ENABLE = LOW;
+            current_mult = MULT_A1;            
             break;
         case MULT_A1://connected to clutch of corresponding controller
+            CONT1_CLUTCH = CONTR_OUT1;
+            CONT2_CLUTCH = CONTR_OUT2;
+            
+            MULT_S0 = LOW;
+            MULT_S1 = HIGH;
+            MULT_S2 = LOW;
+            MULT_ENABLE = LOW;
+            current_mult = MULT_A2;
             break;
         case MULT_A2://connected to gear 6
+            CONT1_GEAR6 = CONTR_OUT1;
+            CONT2_GEAR6 = CONTR_OUT2;
+            
+            MULT_S0 = HIGH;
+            MULT_S1 = HIGH;
+            MULT_S2 = LOW;
+            MULT_ENABLE = LOW;
+            current_mult = MULT_A3;
             break;
         case MULT_A3://connected to gear 5
+            CONT1_GEAR5 = CONTR_OUT1;
+            CONT2_GEAR5 = CONTR_OUT2;
+            
+            MULT_S0 = LOW;
+            MULT_S1 = LOW;
+            MULT_S2 = HIGH;
+            MULT_ENABLE = LOW;
+            current_mult = MULT_A4;
             break;
         case MULT_A4://connected to gear 4
+            CONT1_GEAR4 = CONTR_OUT1;
+            CONT2_GEAR4 = CONTR_OUT2;
+            
+            MULT_S0 = HIGH;
+            MULT_S1 = LOW;
+            MULT_S2 = HIGH;
+            MULT_ENABLE = LOW;
+            current_mult = MULT_A5;
             break;
         case MULT_A5://connected to gear 3
+            CONT1_GEAR3 = CONTR_OUT1;
+            CONT2_GEAR3 =CONTR_OUT2;
+            
+            MULT_S0 = LOW;
+            MULT_S1 = HIGH;
+            MULT_S2 = HIGH;
+            MULT_ENABLE = LOW;
+            current_mult = MULT_A6;
             break;
         case MULT_A6://connected to gear 2
+            CONT1_GEAR2 = CONTR_OUT1;
+            CONT2_GEAR2 = CONTR_OUT2;
+            
+            MULT_S0 = HIGH;
+            MULT_S1 = HIGH;
+            MULT_S2 = HIGH;
+            MULT_ENABLE = LOW;
+            current_mult = MULT_A7;
             break;
         case MULT_A7://connected to gear 1
+            CONT1_GEAR1 = CONTR_OUT1;
+            CONT2_GEAR1 = CONTR_OUT2;
+            
+            MULT_S0 = LOW;
+            MULT_S1 = LOW;
+            MULT_S2 = LOW;
+            MULT_ENABLE = LOW;
+            current_mult = MULT_A0;
             break;
-        
+        case MULT_None:
+            MULT_ENABLE = HIGH;
+            current_mult = MULT_A0;
         default:
             current_mult = MULT_None;
             break;
